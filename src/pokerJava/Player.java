02fa299705177b98cd51df3313e5d23aa49f7609
Björn Bergdahl <molnet@gmail.com>
2019-06-27 10:18:59 +0200

    Added player methods

--- src/pokerJava/Player.java
+++ src/pokerJava/Player.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class Player {
     String name;
     ArrayList<Card> hand = new ArrayList<>();
+
+    public ArrayList<Card> getHand() {
+        ArrayList<Card> dealtHand = new ArrayList<>();
+        return dealtHand;
+    }
+
+    public int evaluateHand() {
+        return 42;
+    }
+
+
 }
